4ac568ef2b94655b113164b48bb7d651d03e7761
vcontreras <contrerastrece@gmail.com>
2023-08-01 01:20:20 -0500

    se agrego la opcion de activar el GPS

---
+++ src/components/MainResults.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import icon from '../assets/img/HeavyRain.png';
+import direction from '../assets/img/direction.svg'
+const MainResults = () => {
+  return (
+    <div className='bg-[#100E1D] md:w-full md:h-[100dvh] overflow-y-auto max-w-[62rem] md:px-[5rem] py-[1rem]'>
+      <div className="flex gap-3 flex-wrap items-center md:justify-between md:gap-5 p-[1.5rem]">
+
+    
+      <div className="w-[7.5rem] bg-[#1E213A] h-[10rem] flex flex-col items-center justify-center">
+        <h2 className='text-[#E7E7EB] text-[1rem]'>Tomorrow</h2>
+        <img src={icon} alt="" className='w-[3.5rem]' />
+        <p className='text-[1rem] text-[#E7E7EB] m-[1rem] flex gap-3'>16 °C <span className='text-[#A09FB1]'>11 °C</span></p>
+      </div>
+      <div className="w-[7.5rem] bg-[#1E213A] h-[10rem] flex flex-col items-center justify-center">
+        <h2 className='text-[#E7E7EB] text-[1rem'>Tomorrow</h2>
+        <img src={icon} alt="" className='w-[3.5rem]' />
+        <p className='text-[1rem] text-[#E7E7EB] m-[1rem] flex gap-3'>16 °C <span className='text-[#A09FB1]'>11 °C</span></p>
+      </div>
+      <div className="w-[7.5rem] bg-[#1E213A] h-[10rem] flex flex-col items-center justify-center">
+        <h2 className='text-[#E7E7EB] text-[1rem]'>Tomorrow</h2>
+        <img src={icon} alt="" className='w-[3.5rem]' />
+        <p className='text-[1rem] text-[#E7E7EB] m-[1rem] flex gap-3'>16 °C <span className='text-[#A09FB1]'>11 °C</span></p>
+      </div>
+      <div className="w-[7.5rem] bg-[#1E213A] h-[10rem] flex flex-col items-center justify-center">
+        <h2 className='text-[#E7E7EB] text-[1rem]'>Tomorrow</h2>
+        <img src={icon} alt="" className='w-[3.5rem]' />
+        <p className='text-[1rem] text-[#E7E7EB] m-[1rem] flex gap-3'>16 °C <span className='text-[#A09FB1]'>11 °C</span></p>
+      </div>
+      <div className="w-[7.5rem] bg-[#1E213A] h-[10rem] flex flex-col items-center justify-center">
+        <h2 className='text-[#E7E7EB] text-[1rem]'>Tomorrow</h2>
+        <img src={icon} alt="" className='w-[3.5rem]' />
+        <p className='text-[1rem] text-[#E7E7EB] m-[1rem] flex gap-3'>16 °C <span className='text-[#A09FB1]'>11 °C</span></p>
+      </div>
+      </div> 
+
+      <div className="p-[1.5rem] pt-0">
+        <h2 className='text-[1.5rem] text-[#E7E7EB] my-[1rem]'>Today's Hightlihts</h2>
+        <div className="cards flex flex-col md:flex-row gap-[2rem]  text-[#E7E7EB] md:flex-wrap">
+          <div className="card md:w-[20rem] h-[12.5rem] md:h-[10rem] bg-[#1E213A] flex flex-col justify-center items-center">
+            <div className="title">Wind status</div>
+            <div className="text-[#E7E7EB] text-[4rem] flex justify-center items-center">7<span className='text-[2.25rem] text-center'>mph</span></div>
+            <div className="flex gap-2"><img src={direction} alt=""  className='w-[1.8rem] rounded-full bg-[#88869D]'/> <span>wsw</span></div>
+          </div>
+
+          <div className="card md:w-[20rem] h-[12.5rem] md:h-[10rem] bg-[#1E213A] flex flex-col justify-center items-center">
+            <h2>Humidity</h2>
+            <div className="text-[#E7E7EB] text-[4rem] flex justify-center items-center">84<span className='text-[2.25rem] text-center'>%</span></div>
+            <div className='flex flex-col'>
+
+            <div className='text-[0.75rem] text-[#A09FB1] flex justify-between w-[15rem]'><span>0</span> <span>50</span> <span>100</span></div>
+          {/* progress bar */}
+            <div className='bg-[#E7E7EB] h-[0.5rem] w-[15rem] rounded-[1rem]'>
+              <div className='bg-[#FFEC65] rounded-[1rem] h-[0.5rem] w-[85%]'></div>
+            </div>
+            <span className='text-[0.75rem] flex justify-end w-[15rem] text-[#A09FB1] '>%</span>
+            </div>
+          </div>
+
+          <div className="bg-[#1E213A] h-[10rem] md:w-[20rem] md:h-[8rem] flex flex-col justify-center items-center">
+            <h3 className='text-[#E7E7EB] text-[1rem]'>Visibility</h3>
+            <p className='text-[4rem]'>6.4<span className='text-[2.25rem]'>miles</span></p>
+          </div>
+          <div className="bg-[#1E213A] h-[10rem] md:w-[20rem] md:h-[8rem] flex flex-col justify-center items-center">
+            <h3 className='text-[#E7E7EB] text-[1rem]'>Air Pressure</h3>
+            <p className='text-[4rem]'>998<span className='text-[2.25rem]'>mb</span></p>
+          </div>
+        </div>
+      <div className="text-[#A09FB1] text-[0.85rem]">created by vcontreras</div>
+      </div>
+
+    </div>
+  )
+}
+
+export default MainResults
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react'
+import location from '../assets/img/location.svg'
+import gps from '../assets/img/gps.svg'
+import shower from '../assets/img/Shower.png'
+import clouds from '../assets/img/Cloud-background.png'
+import WeatherNav from './WeatherNav'
+
+const Sidebar = () => {
+const [isOpenModal, setIsOpenModal] = useState(false);
+
+const [weatherData, setWeatherData] = useState(null);
+const [latitude, setLatitude] = useState(null);
+const [longitude, setLongitude] = useState(null);
+
+const getWeatherData=async()=>{
+  const API_KEY='eaa81cef3e751d0ae1fd812e9323c09d';
+  const url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${API_KEY}&units=metric&lang=sp`;
+  
+  try {
+    const response=await fetch(url);
+    const result=await response.json();
+    console.log(result)
+    setWeatherData(result);
+  } catch (error) {
+    console.error('Error al obtener datos del clima:', error);
+  }      
+
+}
+
+const handleLocation=()=>{  
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(
+      (position)=>{ 
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+    },
+    (error)=>{
+      console.error("Error al obtener la Ubicacion:",error)
+    })
+    
+  }  
+  
+}
+
+useEffect(() => {
+  if(latitude && longitude){    
+    getWeatherData();
+  }
+}, [latitude,longitude]);
+
+
+// Modal-------------------
+const handleOpenModal=()=>{
+  setIsOpenModal(true)
+}
+
+const handleCloseModal=()=>{
+  setIsOpenModal(false)
+}
+
+
+  return (
+    <div className='h-[100dvh] md:h[100%] w-full relative flex flex-col justify-between items-center md:w-[28rem] bg-[#1E213A] p-3 overflow-hidden'>
+      <div className='w-full h-[2.5rem] flex flex-row justify-between overflow-hidden'>
+        <button className=' bg-[#6E707A] px-3 py-3 text-[#E7E7EB] leading-[0rem]' onClick={handleOpenModal}>Search for Places</button>
+        <button className=''><img src={location} alt="img-location" className='h-[2.5rem]' onClick={handleLocation}/></button>
+      </div>
+      <div className='flex items-center justify-center w-[35rem] h-[20rem] overflow-hidden relative' >
+        <div className='bg-center bg-no-repeat bg-cover w-full h-[20rem] absolute' style={{ backgroundImage: `url(${clouds})`,opacity:'0.1'}}></div>
+        <img src={shower} alt="" className='w-[9.5rem] object-contain opacity-100' />
+
+      </div>
+      <div className='text-[#88869D] flex flex-col text-center gap-[1rem]'>
+        <div className="temperature text-[#E7E7EB] text-[9rem] leading-[5rem]">{Math.round(weatherData?.main?.temp)||'15'} <span className='text-[3rem] text-[#A09FB1]'>°C</span></div>
+        <div className="text-[2.5rem] ">{weatherData?.weather[0]?.description||'Shower'}</div>
+        <p className="date">Today · <span>Fri, 5 Jun</span></p>
+        <div className='flex items-center justify-center mb-[2rem]'>
+          <img src={gps} alt="" />
+          <p>{weatherData?.name||'Helsinki'}</p>
+        </div>
+      </div>
+      <WeatherNav onClose={handleCloseModal} isOpen={isOpenModal}/>
+    </div>
+  )
+}
+
+export default Sidebar
